9b0f8a94a2af2f4673620218d92c673e703fda04
chigoox <dikeemmanuel54@gmail.com>
2023-03-25 00:55:05 -0400

    updated height

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -9,7 +9,7 @@ function Home() {
             <div className=' bg-gradient-to-l from-rose-500 via-[#FF194D] flex items-end  to-[#FF194D] h-fit'>
                 <h1 className='font-bold text-[6rem] top-[2.15rem] left-4 relative text-  headerText'>5TH Player</h1>
             </div>
-            <div className=' h-[34.5rem] mt-2'>
+            <div className=' h-[33rem] mt-2'>
 
 
 
